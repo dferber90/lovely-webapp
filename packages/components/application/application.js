32eeee468019c06b1023db3a1467063daf937e3d
Dominik Ferber <dominik.ferber@gmail.com>
2018-03-10 16:46:52 +0100

    add styled-components

--- packages/components/application/application.js
+++ packages/components/application/application.js
@@ -1,4 +1,20 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Button = styled.button`
+  padding: 0.5em;
+  margin: 0.5em;
+  color: palevioletred;
+  background: papayawhip;
+  border: none;
+  border-radius: 3px;
+  outline: 0;
+  cursor: pointer;
+
+  &:hover {
+    background: #ccbfaa;
+  }
+`;
 
 class Toggle extends React.Component {
   constructor(...args) {
@@ -7,13 +23,13 @@ class Toggle extends React.Component {
   }
   render() {
     return (
-      <button
+      <Button
         onClick={() => {
           this.setState(prevState => ({ on: !prevState.on }));
         }}
       >
         {this.state.on ? 'on' : 'off'}
-      </button>
+      </Button>
     );
   }
 }
@@ -22,5 +38,6 @@ export const Application = () => (
   <div>
     Hello there
     <Toggle />
+    {process.env.NODE_ENV}
   </div>
 );
--- packages/server/index.js
+++ packages/server/index.js
@@ -3,32 +3,45 @@ import { Application } from '@wa/components';
 import { renderToString } from 'react-dom/server';
 import Koa from 'koa';
 import serve from 'koa-static';
+import { ServerStyleSheet } from 'styled-components';
 const app = new Koa();
 
 app.use(serve('static'));
 app.use(serve('client'));
 
-const appBundle =
-  process.env.NODE_ENV === 'production'
-    ? `<script src="/${
-        require('./client/stats.json').assetsByChunkName.main
-      }"></script>`
-    : '';
+const html = ({ title, body, styles }) => {
+  const appBundle =
+    process.env.NODE_ENV === 'production'
+      ? `<script src="/${
+          require('./client/stats.json').assetsByChunkName.main
+        }"></script>`
+      : '<!-- client-side app bundle omitted in server-development -->';
+  return `
+    <!DOCTYPE html>
+      <html>
+      <head>
+        <meta charset="utf-8">
+        <link rel="shortcut icon" href="/favicon.png">
+        <title>${title}</title>
+        ${styles}
+      </head>
+      <body>
+        <div id="app">${body}</div>
+        ${appBundle}
+      </body>
+    </html>`;
+};
 
 app.use(async context => {
-  context.body = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <link rel="shortcut icon" href="/favicon.png">
-    <title>Webapp</title>
-  </head>
-  <body>
-    <div id="app">${renderToString(<Application />)}</div>
-    ${appBundle}
-  </body>
-</html>`;
+  const sheet = new ServerStyleSheet();
+  const body = renderToString(sheet.collectStyles(<Application />));
+  const styles = sheet.getStyleTags();
+
+  context.body = html({
+    title: 'Webapp',
+    body,
+    styles,
+  });
 });
 
 app.listen(3000);
