5cee4d2cb5389cf3365d474cf5493556d2e8f8f0
Dominik Ferber <dominik.ferber@gmail.com>
2018-03-27 22:10:00 +0200

    fix test setup

--- packages/client/jest/setup-env.js
+++ packages/client/jest/setup-env.js
@@ -2,3 +2,7 @@ process.env.APP_SECRET = 'app-secret';
 process.env.PRISMA_ENDPOINT = 'prisma-endpoint';
 process.env.PRISMA_SECRET = 'prisma-secret';
 process.env.GRAPHQL_ENDPOINT = 'http://graphql-endpoint.com';
+
+window.CONFIG = {
+  GRAPHQL_ENDPOINT: 'http://graphql-endpoint.com',
+};
