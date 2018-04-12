9425c4db06abac1041adf9481443e44f78d1cc5a
Dominik Ferber <dominik.ferber@gmail.com>
2018-04-12 21:29:32 +0200

    add error handling to login form

--- packages/components/login-form/login-form.js
+++ packages/components/login-form/login-form.js
@@ -5,7 +5,15 @@ import { Redirect } from 'react-router-dom';
 import gql from 'graphql-tag';
 import { Mutation } from 'react-apollo';
 import Cookies from 'cookies-js';
-import { Input, PrimaryButton, Measure, Label } from '@wa/design-system';
+import { Formik } from 'formik';
+import {
+  Input,
+  PrimaryButton,
+  Measure,
+  Label,
+  ErrorMessage,
+  Divider,
+} from '@wa/design-system';
 import { Me } from '../me';
 
 const AUTHENTICATE_USER_MUTATION = gql`
@@ -19,27 +27,46 @@ const AUTHENTICATE_USER_MUTATION = gql`
   }
 `;
 
+const initialValues = {
+  email: 'johndoe@graph.cool',
+  password: 'graphql',
+};
+
+const validate = values => {
+  const errors = {};
+  if (values.email.trim().length === 0) errors.email = { missing: true };
+  if (values.password.trim().length === 0) errors.password = { missing: true };
+  return errors;
+};
+
 export class LoginForm extends React.Component {
   static propTypes = {
     to: PropTypes.string,
   };
-  state = {
-    email: 'johndoe@graph.cool',
-    password: 'graphql',
-  };
 
-  authenticateUser = async login => {
+  authenticateUser = async (login, values, formik) => {
     let response;
     try {
       response = await login({
-        variables: {
-          email: this.state.email,
-          password: this.state.password,
-        },
+        variables: { email: values.email, password: values.password },
       });
     } catch (e) {
-      // eslint-disable-next-line no-alert
-      alert('Invalid credentials');
+      if (e.graphQLErrors) {
+        if (
+          e.graphQLErrors.some(error =>
+            error.message.startsWith('Could not find user with email')
+          )
+        ) {
+          formik.setErrors({ email: { unknown: true } });
+        }
+        if (
+          e.graphQLErrors.some(error => error.message === 'Invalid password')
+        ) {
+          formik.setErrors({ password: { invalid: true } });
+        }
+      } else {
+        formik.setErrors({ unknownError: true });
+      }
       return;
     }
 
@@ -75,38 +102,79 @@ export class LoginForm extends React.Component {
           if (me && me.id) return <Redirect to="/" />;
 
           return (
-            <Measure>
-              <Label htmlFor="login-email">Email</Label>
-              <Input
-                id="login-email"
-                value={this.state.email}
-                placeholder="Email"
-                onChange={e => this.setState({ email: e.target.value })}
-              />
-              <Label htmlFor="login-password" mt={2}>
-                Password
-              </Label>
-              <Input
-                id="login-password"
-                type="password"
-                value={this.state.password}
-                placeholder="Password"
-                onChange={e => this.setState({ password: e.target.value })}
-              />
-              {this.state.email &&
-                this.state.password && (
-                  <Mutation mutation={AUTHENTICATE_USER_MUTATION}>
-                    {login => (
-                      <PrimaryButton
-                        onClick={() => this.authenticateUser(login)}
-                        mt={2}
-                      >
-                        Log in
-                      </PrimaryButton>
-                    )}
-                  </Mutation>
-                )}
-            </Measure>
+            <Mutation mutation={AUTHENTICATE_USER_MUTATION}>
+              {login => (
+                <Formik
+                  initialValues={initialValues}
+                  validate={validate}
+                  onSubmit={(values, formik) =>
+                    this.authenticateUser(login, values, formik)
+                  }
+                  render={formik => (
+                    <form onSubmit={formik.handleSubmit}>
+                      <Measure>
+                        <Label htmlFor="login-email">Email</Label>
+                        <Input
+                          id="login-email"
+                          name="email"
+                          value={formik.values.email}
+                          placeholder="Email"
+                          onChange={formik.handleChange}
+                          onBlur={formik.handleBlur}
+                        />
+                        {formik.touched.email &&
+                          formik.errors.email &&
+                          formik.errors.email.missing && (
+                            <ErrorMessage>Missing</ErrorMessage>
+                          )}
+                        {formik.touched.email &&
+                          formik.errors.email &&
+                          formik.errors.email.unknown && (
+                            <ErrorMessage>
+                              No account with that email address found
+                            </ErrorMessage>
+                          )}
+                        <Label htmlFor="login-password" mt={2}>
+                          Password
+                        </Label>
+                        <Input
+                          id="login-password"
+                          name="password"
+                          type="password"
+                          value={formik.values.password}
+                          placeholder="Password"
+                          onChange={formik.handleChange}
+                          onBlur={formik.handleBlur}
+                        />
+                        {formik.touched.password &&
+                          formik.errors.password &&
+                          formik.errors.password.missing && (
+                            <ErrorMessage>Missing</ErrorMessage>
+                          )}
+                        {formik.touched.password &&
+                          formik.errors.password &&
+                          formik.errors.password.invalid && (
+                            <ErrorMessage>
+                              Oops, seems like you used an invalid password
+                            </ErrorMessage>
+                          )}{' '}
+                        {formik.errors.unknownError && (
+                          <React.Fragment>
+                            <Divider />
+                            <ErrorMessage mt={2}>
+                              An unknown error occurred.
+                            </ErrorMessage>
+                          </React.Fragment>
+                        )}
+                        <PrimaryButton type="submit" mt={2}>
+                          Log in
+                        </PrimaryButton>
+                      </Measure>
+                    </form>
+                  )}
+                />
+              )}
+            </Mutation>
           );
         }}
       </Me>
