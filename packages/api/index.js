3e36f603c7831fdba8729f4f209f3e71458ba12a
Dominik Ferber <dominik.ferber@gmail.com>
2018-05-26 11:43:53 +0200

    disable requireResolversForResolveType warning

--- packages/api/index.js
+++ packages/api/index.js
@@ -10,6 +10,19 @@ const resolvers = { Query, Mutation };
 const server = new GraphQLServer({
   typeDefs: './schema.graphql',
   resolvers,
+  resolverValidationOptions: {
+    // Mute the following warning:
+    //   Type "Node" is missing a "resolveType" resolver. Pass false into
+    //   "resolverValidationOptions.requireResolversForResolveType"
+    //   to disable this warning.
+    // Not sure about the effects this has. The Apollo Docs state
+    //   requireResolversForResolveType will require a resolveType() method for
+    //   Interface and Union types. This can be passed in with the field
+    //   resolvers as __resolveType(). False to disable the warning.
+    // https://www.apollographql.com/docs/graphql-tools/generate-schema.html
+    // (search for  "requireResolversForResolveType" there)
+    requireResolversForResolveType: false,
+  },
   context: req => ({
     ...req,
     db: new Prisma(prismaOptions),
