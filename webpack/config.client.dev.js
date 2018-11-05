460c50fd2220db17bc01280b8e171d2e8a67e9e8
Dominik Ferber <dominik.ferber@gmail.com>
2018-11-05 22:35:57 +0100

    chore: replace webpack-serve with webpack-dev-server

--- webpack/config.client.dev.js
+++ webpack/config.client.dev.js
@@ -1,32 +1,32 @@
 /* eslint-disable import/no-extraneous-dependencies */
-const webpack = require('webpack');
-const path = require('path');
-const runtimeEnv = require('./utils/runtime-env');
-const history = require('connect-history-api-fallback');
-const convert = require('koa-connect');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const HtmlWebpackHarddiskPlugin = require('html-webpack-harddisk-plugin');
+const webpack = require("webpack");
+const path = require("path");
+const history = require("connect-history-api-fallback");
+const convert = require("koa-connect");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackHarddiskPlugin = require("html-webpack-harddisk-plugin");
+const runtimeEnv = require("./utils/runtime-env");
 
-const publicPath = '/assets/';
+const publicPath = "/assets/";
 const outputPath = path.join(
   process.cwd(),
-  'dist-development',
-  'frontend',
-  'assets'
+  "dist-development",
+  "frontend",
+  "assets"
 );
 
 module.exports = {
-  mode: 'development',
-  stats: 'minimal',
-  devtool: 'cheap-module-source-map',
-  context: path.resolve(process.cwd(), 'packages', 'client'),
-  entry: ['@babel/polyfill', './index.js'],
+  mode: "development",
+  stats: "minimal",
+  devtool: "cheap-module-source-map",
+  context: path.resolve(process.cwd(), "packages", "client"),
+  entry: ["@babel/polyfill", "./index.js"],
   output: {
     path: outputPath,
     publicPath,
-    filename: 'bundle.js',
-    pathinfo: true,
+    filename: "bundle.js",
+    pathinfo: true
   },
   module: {
     rules: [
@@ -34,70 +34,69 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
             presets: [
               [
-                '@babel/preset-env',
-                { targets: { browsers: ['last 2 versions', 'safari >= 7'] } },
-              ],
+                "@babel/preset-env",
+                { targets: { browsers: ["last 2 versions", "safari >= 7"] } }
+              ]
             ],
             plugins: [
               "@babel/plugin-transform-react-jsx",
               "@babel/plugin-proposal-class-properties",
               "graphql-tag",
-              ["styled-components", { "ssr": true }],
+              ["styled-components", { ssr: true }],
               "@babel/plugin-syntax-dynamic-import",
               "react-loadable/babel"
-            ],
-          },
-        },
+            ]
+          }
+        }
       },
       {
         test: /\.(png|jpg|gif)$/,
         use: [
           {
-            loader: 'file-loader',
-            options: {},
-          },
-        ],
-      },
-    ],
+            loader: "file-loader",
+            options: {}
+          }
+        ]
+      }
+    ]
   },
   plugins: [
-    new CleanWebpackPlugin(['dist-development/frontend/*']),
+    new CleanWebpackPlugin(["dist-development/frontend/*"]),
     new webpack.DefinePlugin({
-      SERVER: 'false',
-      DEV: 'true',
+      SERVER: "false",
+      DEV: "true"
     }),
     new HtmlWebpackPlugin({
       // We can't serve index.html from "static"
       // as our server (packages/server) also serves the "static" folder.
       // If an "index.html" file was in "static", we'd never run SSR for the
       // main page during development
-      filename: '../static-dev/index.html',
-      template: 'index.template.html',
+      filename: "../static-dev/index.html",
+      template: "index.template.html",
       alwaysWriteToDisk: true,
       config: JSON.stringify({
-        API_ENDPOINT: { ...process.env, ...runtimeEnv }.API_ENDPOINT,
-      }),
+        API_ENDPOINT: { ...process.env, ...runtimeEnv }.API_ENDPOINT
+      })
     }),
     // this plugin provideds the alwaysWriteToDisk option for HtmlWebpackPlugin
-    new HtmlWebpackHarddiskPlugin(),
+    new HtmlWebpackHarddiskPlugin()
   ],
-  serve: {
-    content: [
-      path.join(outputPath, '..', 'static'),
-      path.join(outputPath, '..', 'static-dev'),
+  devServer: {
+    contentBase: [
+      path.join(outputPath, "..", "static"),
+      path.join(outputPath, "..", "static-dev")
     ],
-    hot: {},
-    clipboard: false,
-    dev: { publicPath },
-    add: app => {
+    hot: true,
+    compress: true,
+    after: app => {
       const historyOptions = {
         // ... see: https://github.com/bripkens/connect-history-api-fallback#options
       };
       app.use(convert(history(historyOptions)));
-    },
-  },
+    }
+  }
 };
